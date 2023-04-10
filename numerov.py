d823507d56c32bca749bbff3215a57195b343d55
Sophile <abhinavpb249@gmail.com>
2023-04-10 22:14:38 +0530

    Add files via upload

--- numerov.py
+++ numerov.py
@@ -6,37 +6,30 @@ from numpy.linalg import inv
 import matplotlib.pyplot as plt
 
 
-#potential, desired max energy
+#Define the potential and Energy
 def v(s): return abs(s)
 em = 20.
-#determine grid
 
+#determine grid
 def f(y): return abs(y)-em
 st = float(fsolve(f,em)) #findroot
 ds= 1/(sqrt(2*em))
 n = int(2*((st/ds)+4*pi))
 s = [(-ds*(n+1)/2)+ds*i for i in range(1,n+1)]
-s=np.array(s)
+
 #calculate KE matrix
-
 def one(n,d): return np.diagflat([1 for i in range (n-abs(d))], d)
-B = (one(n,-1)+ 10*one(n,0) +one(n,1))/12.
 A = (one(n,-1)- 2*one(n,0) +one(n,1))/ds**2
+B = (one(n,-1)+ 10*one(n,0) +one(n,1))/12.
 Bi = inv(B)
-K= np.matmul(A, -Bi)/2
+K= np.matmul(-Bi, A)/2
 
-V= np.diagflat([v(s[i]) for i in range(n)])
 #Hamiltonian
-
+V= np.diagflat([v(s[i]) for i in range(n)])
 H= V+K
 eval, evec = eig(H)
 
-
-i=0
-while i<n:   #Denormalize the vector
-    evec[:,i]= evec[:,i]/evec[:,i][-1]
-    i+=1
-    
+#Denormalize the vector
 evec = np.transpose(evec)
 
 #Show the plot
